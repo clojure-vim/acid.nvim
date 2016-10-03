5e744905b581a078a298b7448685a7aeaae91e2f
Henry Kupty <hkupty@gmail.com>
2016-10-03 14:36:49 -0300

    Testing concept

--- rplugin/python3/acid/__init__.py
+++ rplugin/python3/acid/__init__.py
@@ -1,6 +1,11 @@
 # encoding:utf-8
 """ Acid stands for Asynchronous Clojure Interactive Development. """
 import neovim
+from acid.base import send
+
+
+def ignore(queue):
+    [_ for i in queue]
 
 @neovim.plugin
 class Acid(object):
@@ -8,3 +13,7 @@ class Acid(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
+
+    @neovim.function("AcidEval")
+    def acid_eval(self, data):
+        send(data, ignore)
