a9f85dbabe38068b0f038c218cc87dd5c4fe2c36
Henry Kupty <hkupty@gmail.com>
2016-10-03 17:49:25 -0300

    Add deoplete source

---
+++ rplugin/python3/deoplete/sources/acid.py
@@ -0,0 +1,61 @@
+"""Shameless copy from async-clj-omni.
+
+Sorry.
+"""
+from acid.base import send
+from acid.nvim import get_port_no
+from .base import Base
+import nrepl
+
+
+short_types = {
+    "function": "f",
+    "macro": "m",
+    "var": "v",
+    "special-form": "s",
+    "class": "c",
+    "keyword": "k",
+    "local": "l",
+    "namespace": "n",
+    "field": "i",
+    "method": "f",
+    "static-field": "i",
+    "static-method": "f",
+    "resource": "r"
+}
+
+
+def candidate(val):
+    arglists = val.get("arglists")
+    type = val.get("type")
+    return {
+        "word": val.get("candidate"),
+        "kind": short_types.get(type, type),
+        "info": val.get("doc", ""),
+        "menu": " ".join(arglists) if arglists else ""
+    }
+
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+        self.name = "acid"
+        self.mark = "CLJ"
+        self.filetypes = ['clojure']
+        self.rank = 200
+
+    def gather_candidates(self, context):
+        def handler(queue):
+            return [candidate(i)
+                    for i in filter(
+                            lambda k: "completions" in k,
+                            queue)]
+
+        return send(
+            get_port_no,
+            handler,
+            **{"op": "complete",
+               "symbol": context["complete_str"],
+               "extra-metadata": ["arglists", "doc"],
+               "ns": ns}
+        )
