dad2c2cf11fe6b283e64d4a3349c6a2a92b2a0f4
Henry Kupty <hkupty@gmail.com>
2016-12-17 12:14:49 -0200

    Make internal cmd

--- rplugin/python3/acid/handlers/usage.py
+++ rplugin/python3/acid/handlers/usage.py
@@ -1,24 +1,25 @@
 from acid.handlers import BaseHandler, WithFSM
 from acid.nvim import current_file, current_path
 
-def transform(msg):
-    return {
-        'op': 'find-symbol',
-        'dir': current_path(self.nvim),
-        'file': current_file(self.nvim),
-        'ns': msg['ns'],
-        'name': msg['name'],
-        'line': msg.get('line', 1),
-        'column': msg.get('column', 1),
-        'serialization-format': 'bencode'
-    }
-
 class Handler(WithFSM):
 
     name = 'Usage'
 
+    def transform(self, msg):
+        return {
+            'op': 'find-symbol',
+            'dir': current_path(self.nvim),
+            'file': current_file(self.nvim),
+            'ns': msg['ns'],
+            'name': msg['name'],
+            'line': msg.get('line', 1),
+            'column': msg.get('column', 1),
+            'serialization-format': 'bencode'
+        }
+
+
     def handle_init(self, msg, *_):
-        new_data = transform(msg)
+        new_data = self.transform(msg)
         self.change_state('usage', new_data)
 
     def handle_usage(self, *_):
