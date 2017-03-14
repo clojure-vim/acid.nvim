f6405197ef5e6990a1c3630fc1743c0e21acd6a1
Henry Kupty <hkupty@gmail.com>
2017-03-14 18:50:44 -0300

    Fix typo

--- rplugin/python3/acid/handlers/log.py
+++ rplugin/python3/acid/handlers/log.py
@@ -19,5 +19,5 @@ class Handler(SingletonHandler):
         self.logger.setLevel(logging.DEBUG)
 
     def on_handle(self, msg, *_):
-        if not 'sate' in msg.get('status', []):
+        if not 'state' in msg.get('status', []):
             self.logger.info(msg)
