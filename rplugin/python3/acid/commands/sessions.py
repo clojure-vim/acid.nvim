67c1b4ef19a14bf5fb2a0f09c68862e5eb40fec0
Henry Kupty <hkupty@gmail.com>
2017-03-16 18:03:35 -0300

    Fix AcidSession

--- rplugin/python3/acid/commands/sessions.py
+++ rplugin/python3/acid/commands/sessions.py
@@ -5,22 +5,10 @@ from acid.nvim import path_to_ns
 class Command(BaseCommand):
 
     name = 'Session'
-    nargs = 1
     cmd_name = 'AcidSession'
+    op = 'clone'
     handlers = ['SessionAdd']
 
-    def shorthand(self):
-        pre = self.nvim.funcs.getreg('s')
-        self.nvim.command('normal! "syiw')
-        data = self.nvim.funcs.getreg('s')
-        self.nvim.funcs.setreg('s', pre)
-        return data
-
-    def prepare_payload(self, mode, *args):
-        ns = path_to_ns(self.nvim)
-        if mode == 'shorthand':
-            data = self.shorthand()
-        else:
-            data = mode
-        return {"symbol": data, "ns": ns}
+    def prepare_payload(self, *args):
+        return {}
 
