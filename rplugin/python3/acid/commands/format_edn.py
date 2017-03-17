ab193cd84ae938a425d6c6ec36650822bfd9b4db
Henry Kupty <hkupty@gmail.com>
2017-03-17 16:59:57 -0300

    Git add format_edn command

---
+++ rplugin/python3/acid/commands/format_edn.py
@@ -0,0 +1,29 @@
+from acid.commands import BaseCommand
+from acid.nvim import get_acid_ns
+
+
+class Command(BaseCommand):
+
+    name = 'FormatEdn'
+    cmd_name = 'AcidFormatEdn'
+    handlers = ['MetaRepl']
+    mapping = 'cf'
+    opfunc = True
+    nargs='*'
+    op = "format-edn"
+
+    def prompt(self):
+        self.nvim.call("inputsave")
+        ret = self.nvim.call("input", "acid - format-edn> ")
+        self.nvim.call("inputrestore")
+        return ret
+
+    def prepare_payload(self, mode, *args):
+        if mode == 'prompt':
+            ret = self.prompt()
+        elif mode == 'opfunc':
+            ret = " ".join(args)
+        else:
+            ret = " ".join([mode, *args])
+
+        return {"edn": ret}
