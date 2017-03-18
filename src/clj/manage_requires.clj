d8509aa6d0b60b1878c5b7bda9125f71776ba848
Henry Kupty <hkupty@gmail.com>
2017-03-18 20:53:08 -0300

    Add clojure code to inject

---
+++ src/clj/manage_requires.clj
@@ -0,0 +1,12 @@
+(defn manipulate-req [reqs modify]
+  (let [[[h] v] (split-at 1 reqs)]
+    (conj (sort-by first (modify v)) h)))
+
+(defn sort-reqs [reqs]
+  (manipulate-req reqs identity))
+
+(defn add-req [reqs new-req]
+  (manipulate-req reqs #(conj % new-req)))
+
+(defn rem-req [reqs new-req]
+  (manipulate-req reqs (partial remove #(= new-req (first %)))))
