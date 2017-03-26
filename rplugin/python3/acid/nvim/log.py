126b06031f5eee138a4a8f92c3f706136e485171
Henry Kupty <hkupty@gmail.com>
2017-03-26 17:21:43 -0300

    Get name

--- rplugin/python3/acid/nvim/log.py
+++ rplugin/python3/acid/nvim/log.py
@@ -10,7 +10,7 @@ fh.setFormatter(formatter)
 
 
 def _log(frame, fn):
-    name = inspect.getmodule(frame[0])
+    name = inspect.getmodule(frame[0]).__name__
     logger = logging.getLogger(name)
     logger.addHandler(fh)
     logger.setLevel(logging.DEBUG)
