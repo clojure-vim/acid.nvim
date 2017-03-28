2515d1bb8823c4f4488514e101e29bb0537142db
Henry Kupty <hkupty@gmail.com>
2017-03-27 21:17:12 -0300

    Remove debug log

--- rplugin/python3/acid/commands/__init__.py
+++ rplugin/python3/acid/commands/__init__.py
@@ -123,10 +123,6 @@ class BaseCommand(object):
             acid.nvim, handlers_var, inst.handlers
         )
 
-        log.log_debug([
-            str(custom), handlers_var, str(inst.handlers)
-        ])
-
         handlers = map(lambda h: inst.start_handler(
             context, context['handlers'].get(h)), custom
         )
