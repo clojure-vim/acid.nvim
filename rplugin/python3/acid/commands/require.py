ecbc611c0df7dae4a378f2ccae9ffa27572f9e0a
Henry Kupty <hkupty@gmail.com>
2017-04-03 12:39:59 -0300

    allow require to have  as args

--- rplugin/python3/acid/commands/require.py
+++ rplugin/python3/acid/commands/require.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
                 )
                 return None
         else:
-            ns = args[0]
+            ns = " ".join(args)
 
         return {"code": "(require '[{}] :reload)".format(ns)}
 
