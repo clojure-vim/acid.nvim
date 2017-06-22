94f9076da869d2448c8a2c415a5b000cb014f988
Henry Kupty <hkupty@gmail.com>
2017-06-22 19:40:25 -0300

    Add AcidRename command

---
+++ rplugin/python3/acid/handlers/mv.py
@@ -0,0 +1,15 @@
+from acid.handlers import BaseHandler
+from acid.nvim.log import echo_warning
+
+class Handler(BaseHandler):
+
+    name = "MvSingle"
+    priority = 0
+
+    def on_handle(self, msg, *_):
+        if 'error' in msg:
+            echo_warning(self.nvim, msg['error'])
+        else:
+            path = msg['touched'][0]
+            self.nvim.command('edit {}'.format(path))
+
