eecbcdfb0f59bef23c7ddcd38cfbe73b8da269a3
Henry Kupty <hkupty@gmail.com>
2017-07-24 17:22:14 -0300

    More logs

--- rplugin/python3/acid/commands/doc.py
+++ rplugin/python3/acid/commands/doc.py
@@ -1,5 +1,5 @@
 from acid.commands import BaseCommand
-from acid.nvim import path_to_ns
+from acid.nvim import path_to_ns, log
 from acid.pure.doc_fmt import doc_transform
 from collections import OrderedDict
 
@@ -58,6 +58,7 @@ class Command(BaseCommand):
     shorthand="call setreg('s', expand('<cword>'))"
 
     def on_configure(self, *_a, **_k):
+        log.log_debug('Passing doc transformation fn')
         return [doc_transform(definition)]
 
     def prepare_payload(self, data):
