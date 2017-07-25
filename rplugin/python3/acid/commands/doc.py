86f5f8704304a9ea755d1190be538db4b1cb8abc
Henry Kupty <hkupty@gmail.com>
2017-07-25 13:13:54 -0300

    Fix doc

--- rplugin/python3/acid/commands/doc.py
+++ rplugin/python3/acid/commands/doc.py
@@ -16,7 +16,7 @@ definition = {
                 for i in t[2:-2].split('] [')
             ])
         }],
-        ['doc', {'default': '', 'transform': '\n'.split}],
+        ['doc', {'default': '', 'transform': lambda k: k.split('\n')}],
         ['javadoc', {'default': ''}],
         ['added', {'default': '', 'transform': 'Since version: {}'.format}],
         ['super', {'default': '', 'transform': 'Extends: {}'.format}],
