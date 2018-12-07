51524be47af94b8e4659fcee49a8141ba957c121
Henry Kupty <hkupty@gmail.com>
2018-12-07 16:20:32 +0100

    Prepare to migrate commands/handlers to lua
    This would allow a much better and simpler implementation of either

---
+++ lua/acid/connections.lua
@@ -0,0 +1,42 @@
+-- luacheck: globals vim
+local nvim = vim.api
+local connections = {
+  store = {},
+  current = {},
+}
+
+connections.add = function(this, addr)
+  table.insert(this.store, addr)
+  return #this.store
+end
+
+connections.select = function(this, pwd, ix)
+  this.current[pwd] = ix
+end
+
+connections.unselect = function(this, pwd)
+  this.current[pwd] = nil
+end
+
+connections.get = function(this, pwd)
+  local ix = this.current[pwd]
+
+  if ix == nil then
+    local port_file = pwd .. '/.nrepl-port'
+    if nvim.nvim_call_function('filereadable', {port_file}) then
+      local port = nvim.nvim_call_function('readfile', {port_file})
+      ix = this:add({'127.0.0.1', port[1]})
+      this:select(pwd, ix)
+    else
+      if #this.store >= 1 then
+        ix = #this.store
+      else
+        return
+      end
+    end
+  end
+
+  return this.store[ix]
+end
+
+return connections
--- lua/acid/init.lua
+++ lua/acid/init.lua
@@ -1,23 +1,16 @@
--- luacheck: globals unpack vim
-local nvim = vim.api
+local commands = require("acid.commands")
+local handlers = require("acid.handlers")
+local core = require("acid.core")
 
-local indirection = {}
-local acid = {}
+local acid = {
+  commands = commands,
+  handlers = handlers
+}
 
-acid.send = function(obj, handler)
-  local session = math.random(10000, 99999)
-
-  indirection[session] = handler
-
-  nvim.nvim_call_function("AcidSendNrepl", {
-      obj,
-      "LuaFn",
-      "require('acid').callback(" .. session .. ", _A)"
-    })
-end
+acid.run = function(data, conn) return core.send(conn, data()) end
 
 acid.callback = function(session, ret)
-  return indirection[session](ret)
+  return core.indirection[session](ret)
 end
 
 return acid
