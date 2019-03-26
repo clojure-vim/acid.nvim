063a42b54989bcc0282535a2dbf7fc102d7439e9
Henry Kupty <hkupty@gmail.com>
2019-03-26 17:32:19 +0100

    #41: Add .nrepl-port from lua if it's found

--- lua/acid/core.lua
+++ lua/acid/core.lua
@@ -4,6 +4,7 @@
 -- @module acid.core
 -- @todo merge with acid and acid.connections
 local connections = require("acid.connections")
+local utils = require("acid.utils")
 
 local core = {
   indirection = {}
@@ -20,8 +21,20 @@ core.send = function(conn, obj, handler)
   end
 
   local session = math.random(10000, 99999)
+  local pwd = vim.api.nvim_call_function("getcwd", {})
 
-  conn = conn or connections:get(vim.api.nvim_call_function("getcwd", {}))
+  conn = conn or connections:get(pwd)
+  local new_conn
+
+  if conn == nil then
+    local fpath = vim.api.nvim_call_function("findfile", {".nrepl-port"})
+    if fpath == "" then
+      return
+    end
+    local portno = table.concat(vim.api.nvim_call_function("readfile", {fpath}), "")
+    conn = {"127.0.0.1", utils.trim(portno)}
+    new_conn = true
+  end
 
   core.indirection[session] = {
     fn = handler,
