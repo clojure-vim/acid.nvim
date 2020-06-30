da42ca595c74c2c5b4a362e6cbe7117d66c6c564
Henry Kupty <hkupty@gmail.com>
2020-06-30 18:04:17 +0200

    Allow supplying connection id or ip-port tuple

--- lua/acid/core.lua
+++ lua/acid/core.lua
@@ -15,7 +15,8 @@ local core = {
 -- @tparam[opt] {string,string} conn Ip and Port tuple. Will try to get one if nil.
 -- @tparam table obj Payload to be sent to the nrepl.
 -- @tparam function handler Handler function to deal with the response.
-core.send = function(conn, obj, handler)
+core.send = function(connection, obj, handler)
+  local conn, conn_id
   if handler == nil then
     vim.api.nvim_err_writeln("Please provide a handler for that operation.")
     return
@@ -23,7 +24,17 @@ core.send = function(conn, obj, handler)
 
   local pwd = vim.api.nvim_call_function("getcwd", {})
 
-  conn = conn or connections.attempt_get(pwd)
+  local ctp = type(connection)
+  if ctp == "string" then
+    conn_id = connection
+    conn = connections.store[connection]
+  elseif ctp == "table" then
+    conn = connection
+    conn_id = connections.reverse_lookup(connection)
+  elseif connection == nil then
+    conn_id = connections.peek()
+    conn = connections.store[conn_id]
+  end
 
   if conn == nil then
     log.msg("No active connection to a nrepl session. Aborting")
