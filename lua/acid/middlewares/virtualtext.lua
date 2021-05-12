fa0bf228c91a5d02f0fd227b87a352582f3d5f05
Henry Kupty <hkupty@gmail.com>
2021-05-12 13:19:56 +0200

    Output improvements

--- lua/acid/middlewares/virtualtext.lua
+++ lua/acid/middlewares/virtualtext.lua
@@ -1,48 +1,53 @@
 -- luacheck: globals vim
 local virtualtext = {}
+local forms = require("acid.forms")
 
 virtualtext.name = "virtualtext"
-
-virtualtext.cache_index = {}
-virtualtext.cache = {}
 
 virtualtext.ns = vim.api.nvim_create_namespace("acid")
 
 virtualtext.toggle = function()
   local cb = vim.api.nvim_get_current_buf()
-  local ln = vim.api.nvim_call_function("line", {"."}) - 1
-  local key = tostring(cb) .. "/" .. tostring(ln)
+  local coords = forms.get_form_boundaries(true)
+
+  local extmarks = vim.api.nvim_buf_get_extmarks(cb, virtualtext.ns, coords.from, coords.to, {details = true})
+
+  local nxt = extmarks[1]
+
+  nxt[4].priority = 10
+  nxt[4].id = nxt[1]
+
+  -- TODO fix
+  nxt[4].end_line = nxt[4].end_row
+  nxt[4].end_row = nil
+
+  vim.api.nvim_buf_set_extmark(
+    0,
+    virtualtext.ns,
+    nxt[2],
+    nxt[3] ,
+    nxt[4]
+    )
 
 
-  local possible_vts = virtualtext.cache[key]
-  local current_ix = virtualtext.cache_index[key]
+  --if #possible_vts > 1 then
+    --current_ix = current_ix - 1
+    --if current_ix == 0 then
+      --current_ix = #possible_vts
+    --end
+  --end
 
-  if #possible_vts > 1 then
-    current_ix = current_ix - 1
-    if current_ix == 0 then
-      current_ix = #possible_vts
-    end
-  end
-
-  vim.api.nvim_buf_set_virtual_text(cb, virtualtext.ns, ln, possible_vts[current_ix], {})
-  vim.api.nvim__buf_redraw_range(cb, ln, ln)
-  virtualtext.cache_index[key] = current_ix
+  --vim.api.nvim_buf_set_virtual_text(cb, virtualtext.ns, ln, possible_vts[current_ix], {})
+  --vim.api.nvim__buf_redraw_range(cb, ln, ln)
+  --virtualtext.cache_index[key] = current_ix
 
 end
 
 virtualtext.middleware = function(config)
   return function(middleware)
     return function(data)
-      local cb = vim.api.nvim_get_current_buf()
-      local ln = config.to[1] - 1
-      local key = tostring(cb) .. "/" .. tostring(ln)
-
       if data.status then
         return
-      end
-
-      if virtualtext.cache[key] == nil then
-        virtualtext.cache[key] = {}
       end
 
       local vt = {}
@@ -61,13 +66,21 @@ virtualtext.middleware = function(config)
 
       if #vt > 0 then
         table.insert(vt, 1, {";; => ", "Comment"})
-        table.insert(virtualtext.cache[key], vt)
 
-        virtualtext.cache_index[key] = #virtualtext.cache[key]
+        vim.api.nvim_buf_set_extmark(
+          0,
+          virtualtext.ns,
+          config.from[1] - 1,
+          config.from[2] - 1 ,
+          {
+            end_line = config.to[1] - 1,
+            end_col = config.to[2] - 1,
+            virt_text = vt
+        })
 
         -- TODO split_lines
-        vim.api.nvim_buf_set_virtual_text(cb, virtualtext.ns, ln, vt, {})
-        vim.api.nvim__buf_redraw_range(cb, ln, ln)
+        --vim.api.nvim_buf_set_virtual_text(cb, virtualtext.ns, ln, vt, {})
+        --vim.api.nvim__buf_redraw_range(cb, ln, ln)
 
       end
       return middleware(data)
